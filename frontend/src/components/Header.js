d0247b5054ed37c575256b1a1ba9e0b67347f538
techbysj <jsabood19@gmail.com>
2025-01-06 12:53:46 +0000

    Header Created

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import Logo from './Logo'
 
 const Header = () => {
   return (
-    <div>
-      header
-    </div>
-  )
+    <header className="h-16 shadow-md">
+      <div className="h-full container mx-auto flex items-center px-2 justify-between">
+        <Logo w={120} h={60} />
+
+        <div>Search</div>
+
+        <div>user and Cart icon</div>
+      </div>
+    </header>
+  );
 }
 
 export default Header
