e06795e1514422a55398ce149541b8877e4266a6
techbysj <jsabood19@gmail.com>
2025-01-06 12:28:51 +0000

    Footer and header Setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
+import { Outlet } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      Mern App
-    </div>
+    <>
+      <Header />
+      <Outlet/>
+      <Footer/>
+    </>
   );
 }
 
