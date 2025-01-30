fca324a6b3cbad3e5587b49dec33b5a5d2e1f92f
techbysj <jsabood19@gmail.com>
2025-01-30 13:24:49 +0000

    Sign Up Page created

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,18 +1,52 @@
-import React from 'react'
-import Logo from './Logo'
+import React from "react";
+import Logo from "./Logo";
+import { FaSearch } from "react-icons/fa";
+import { FaUser } from "react-icons/fa";
+import { FaShoppingCart } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header className="h-16 shadow-md">
+    <header className="h-16 shadow-md bg-white">
       <div className="h-full container mx-auto flex items-center px-2 justify-between">
-        <Logo w={120} h={60} />
+         <Link to="/">
+          <Logo w={120} h={60} />
+        </Link>
 
-        <div>Search</div>
+        <div className="hidden lg:flex items-center w-full justify-between max-w-sm border rounded-full focus-within:shadow-md pl-4">
+          <input
+            type="text"
+            placeholder="Search Product Here"
+            className="w-full outline-none "
+          />
+          <div className=" text-lg min-w-[50px] h-8 bg-red-500 flex items-center justify-center rounded-r-full text-white">
+            <FaSearch />
+          </div>
+        </div>
 
-        <div>user and Cart icon</div>
+        <div className="flex items-center gap-8">
+          <div>
+            <FaUser className="text-2xl cursor-pointer " />
+          </div>
+          <div>
+            <span>
+              <FaShoppingCart className="text-2xl cursor-pointer  relative" />
+            </span>
+            <div className="bg-red-500 w-5 h-5 rounded-full flex items-center justify-center ml-4 text-white absolute top-2">
+              <p className="text-sm">0</p>
+            </div>
+          </div>
+          <div>
+            <Link to="/login">
+            <button className="bg-red-500 text-white  hover:bg-red-300 px-4 py-2 rounded-full">
+              Login
+            </button>
+            </Link>
+          </div>
+        </div>
       </div>
     </header>
   );
-}
+};
 
-export default Header
+export default Header;
---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,95 @@
+import React, {useState} from "react";
+import loginIcons from "../assest/signin.gif";
+import { FaEye } from "react-icons/fa";
+import { FaEyeSlash } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [data, setData] = useState({
+    email: "",
+    password: ""
+  })
+
+  const handleOnChange = (e) =>{
+    const {name, value} = e.target
+
+    setData((preve)=>{
+        return {
+        ...preve,
+        [name]: value
+        }
+  })
+  }
+
+  console.log("data login", data)
+
+
+  return (
+    <section id="login">
+      <div className="mx-auto container p-7">
+        <div className="bg-white p-4 py-5 w-full max-w-md mx-auto">
+          <div className="w-20 h-20 mx-auto">
+            <img src={loginIcons} alt="loginIcon" />
+          </div>
+
+          <form className="w-full max-w-md mx-auto pt-8">
+            <div className="grid">
+              <label>Email</label>
+            </div>
+            <div className="bg-slate-200 p-4">
+              <input
+                type="email"
+                placeholder="Enter Email"
+                className="w-full h-full outline-none bg-transparent"
+                name="email"
+                value={data.email}
+                onChange={handleOnChange}
+
+              />
+            </div>
+            <div>
+              <label>Password</label>
+            </div>
+            <div className="bg-slate-200 p-4 flex">
+              <input
+                type={showPassword? "": "password"}
+                placeholder="Enter Password"
+                className="w-full h-full outline-none bg-transparent"
+                name="password"
+                value={data.password}
+                onChange={handleOnChange}
+              />
+              <div className="flex justify-between items-center">
+                <div className="cursor-pointer">
+                 {
+                     showPassword? <FaEyeSlash onClick={() => setShowPassword(false)} /> : <FaEye onClick={() => setShowPassword(true)} />
+                 }
+                </div>
+              </div>
+               
+            </div>
+            <div className="flex justify-between items-center">
+              <div>
+                <input type="checkbox" />
+                <label className="ml-2">Remember Me</label>
+              </div>
+              <div>
+                <Link to="/forgot-password" className="block w-fit ml-auto hover:underline hover:text-red-400">Forgot Password?</Link>
+              </div>
+            </div>
+            <button type="submit" className="bg-red-500 hover:bg-red-700 text-white px-6 py-2 w-full max-w-[140px] rounded-full hover:scale-105 ">
+                Login
+            </button>
+          </form>
+            <div className="flex justify-center items-center mt-4">
+                <p className="mr-2">Don't have an account?</p>
+                <Link to={"/sign-up"} className="hover:underline text-red-400 hover:text-red-500">Sign Up</Link>
+                </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Login;
