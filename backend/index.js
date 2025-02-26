63295701319dbfd11644f49537e51294252635ca
techbysj <jsabood19@gmail.com>
2025-02-26 12:28:56 +0000

    Created Backend

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+const express = require ("express")
+const cors = require("cors")
+require('dotenv').config()
+const connectDB = require("./config/db")
+
+
+const app = express()
+app.use(cors())
+
+const PORT = 8088 || process.env.PORT
+
+connectDB()
+
+app.listen(PORT, ()=>{
+    console.log("Server is running")
+})
+
+
+
+
+
