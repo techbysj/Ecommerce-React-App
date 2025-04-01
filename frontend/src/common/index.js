dca04f38b19dd43b92b081184dd12641d5b73726
techbysj <jsabood19@gmail.com>
2025-04-01 19:38:19 +0100

    `Added new API endpoints and CategoryList component to Home page`

--- frontend/src/common/index.js
+++ frontend/src/common/index.js
@@ -38,6 +38,46 @@ const SummaryApi = {
         url : `${backendDomain}/api/update-product`,
         method : 'POST'
     },
+    categoryProduct : {
+        url : `${backendDomain}/api/get-categoryProduct`,
+        method : 'get'
+    },
+    categoryWiseProduct : {
+        url : `${backendDomain}/api/category-product`,
+        method : 'post'
+    },
+    productDetails : {
+        url : `${backendDomain}/api/product-details`,
+        method : 'post'
+    },
+    addToCartProduct : {
+        url : `${backendDomain}/api/addtocart`,
+        method : 'post'
+    },
+    addToCartProductCount : {
+        url : `${backendDomain}/api/countAddToCartProduct`,
+        method : 'get'
+    },
+    addToCartProductView : {
+        url : `${backendDomain}/api/view-card-product`,
+        method : 'get'
+    },
+    updateCartProduct : {
+        url : `${backendDomain}/api/update-cart-product`,
+        method : 'post'
+    },
+    deleteCartProduct : {
+        url : `${backendDomain}/api/delete-cart-product`,
+        method : 'post'
+    },
+    searchProduct : {
+        url : `${backendDomain}/api/search`,
+        method : 'get'
+    },
+    filterProduct : {
+        url : `${backendDomain}/api/filter-product`,
+        method : 'post'
+    }
 
 }
 
