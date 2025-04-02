56e1f429f867b0a59caf01df38bd75a733315543
techbysj <jsabood19@gmail.com>
2025-04-02 03:07:10 +0100

    `Update frontend code to reflect new project name and branding`

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,8 +3,11 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className="bg-slate-400">
-      <div className='container mx-auto p-4'>
-        <p className='text-center'>&copy; 2021 E-commerce. All Rights Reserved.</p>
+      <div className="container mx-auto p-4">
+        <p className="text-center">
+          Drafted with Love by <strong>TECHBYSJ</strong> &copy; 2030 E-commerce.
+          All Rights Reserved.
+        </p>
       </div>
     </footer>
   );
