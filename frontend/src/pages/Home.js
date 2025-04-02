f93e94d9bb39b38c492be0c4e9b7f892fcd2df16
techbysj <jsabood19@gmail.com>
2025-04-02 14:22:12 +0100

    `Removed and commented out VerticalCardProduct components`

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -11,25 +11,25 @@ const Home = () => {
       <BannerProduct />
 
       <HorizontalCardProduct category={"airpodes"} heading={"Top's Airpodes"} />
+      <VerticalCardProduct category={"mobiles"} heading={"Mobiles"} />
       <HorizontalCardProduct
         category={"watches"}
         heading={"Popular's Watches"}
       />
 
-      <VerticalCardProduct category={"mobiles"} heading={"Mobiles"} />
-      <VerticalCardProduct category={"Mouse"} heading={"Mouse"} />
-      <VerticalCardProduct category={"televisions"} heading={"Televisions"} />
+      {/* <VerticalCardProduct category={"Mouse"} heading={"Mouse"} /> */}
+      {/* <VerticalCardProduct category={"televisions"} heading={"Televisions"} /> */}
       <VerticalCardProduct
         category={"camera"}
         heading={"Camera & Photography"}
       />
       <VerticalCardProduct category={"earphones"} heading={"Wired Earphones"} />
-      <VerticalCardProduct
+      {/* <VerticalCardProduct
         category={"speakers"}
         heading={"Bluetooth Speakers"}
-      />
-      <VerticalCardProduct category={"refrigerator"} heading={"Refrigerator"} />
-      <VerticalCardProduct category={"trimmers"} heading={"Trimmers"} />
+      /> */}
+      {/* <VerticalCardProduct category={"refrigerator"} heading={"Refrigerator"} />
+      <VerticalCardProduct category={"trimmers"} heading={"Trimmers"} /> */}
     </div>
   );
 };
